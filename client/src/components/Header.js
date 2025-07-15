fb6cdb57a477c52ad513d38ad361513e5602397f
Boswecw <charliewboswell@gmail.com>
2025-07-15 13:13:34 -0400

    missing header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Furbabies</h1>
+      {/* Add your header content here */}
+    </header>
+  );
+};
+
+export default Header;
